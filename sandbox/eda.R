043bb7a91c6c046054baf307b351815b0d497477
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-10-20 21:01:26 +0200

    Add time-series plot for R repos

---
+++ sandbox/eda.R
@@ -0,0 +1,17 @@
+pkgload::load_all()
+
+library(tidyverse)
+library(hrbrthemes)
+
+r_repos |>
+  ggplot(aes(x = date, y = r_repos)) +
+  geom_line(linewidth = 1, colour = "#1d65b7") +
+  geom_area(fill = "#1d65b7", alpha = .2) +
+  theme_ipsum() +
+  labs(
+    title = "The number of R repositories on GitHub is steadily climbing",
+    x = NULL,
+    y = "Number of repos"
+  ) +
+  scale_x_date(date_breaks = "2 year", date_labels = "%Y")
+
