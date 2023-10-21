b0fea090f26748cac35a107e9e089ecc2041e2dd
MikeJohnPage <38110953+MikeJohnPage@users.noreply.github.com>
2023-10-21 10:49:17 +0200

    Move file and wrap fun

---
+++ R/gh.R
@@ -0,0 +1,50 @@
+query_gh <- function(endpoint) {
+  Sys.sleep(60 / 30)
+  response <- gh::gh(
+    endpoint = endpoint,
+    .limit = 1
+  )
+  response$total_count
+}
+
+repos_monthly <- function(start_date = "2010-01-01", end_date = "today", ...) {
+  if (is.na(lubridate::as_date(start_date, format = "%Y-%m-%d"))) {
+    stop("Please supply a `start_date` in the format %Y-%m-%d")
+  } else {
+    start_date <- lubridate::ymd(start_date)
+  }
+
+  if (end_date == "today") {
+    end_date <- lubridate::today()
+  } else if (is.na(lubridate::as_date(end_date, format = "%Y-%m-%d"))) {
+    stop("Please supply a `end_date` in the format %Y-%m-%d")
+  } else {
+    end_date <- lubridate::ymd(end_date)
+  }
+
+  if (start_date > end_date) {
+    stop("`start_date` must be before `end_date`")
+  }
+
+  start_dates <- seq(
+    from = start_date,
+    to = lubridate::floor_date(end_date, "month") - 1,
+    by = "month"
+  )
+
+  end_dates <- lubridate::ceiling_date(start_dates, "month") - 1
+
+  params <- paste0("language:R created:", start_dates, "..", end_dates)
+
+  endpoint <- paste0(
+    "/search/repositories?q=",
+    params # encoded by gh::gh()
+  )
+
+  count <- purrr::map_int(endpoint, query_gh, .progress = TRUE)
+
+  tibble::tibble(
+    date = end_dates,
+    repo_count = count
+  )
+}
